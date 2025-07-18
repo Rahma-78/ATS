97cf3b8f31917606d5fa483403ac28797c2d34d4
Rahma-Alshafi <rahmaelshafe78@gmail.com>
2025-07-18 18:24:51 +0300

    path

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ logging.basicConfig(
 )
 
 # --- FastAPI App Initialization ---
-root_path = os.environ.get("ROOT_PATH", "")
+root_path = ""
 app = FastAPI(
     title=settings.PROJECT_NAME,
     description=settings.PROJECT_DESCRIPTION,
