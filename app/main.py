02d72d541492adce569e2f6491da45e7c5729f18
Rahma-Alshafi <rahmaelshafe78@gmail.com>
2025-07-19 17:32:29 +0300

    paths

--- app/main.py
+++ app/main.py
@@ -56,7 +56,7 @@ templates = Jinja2Templates(directory="app/static")
 # --- Root Endpoint ---
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "url_for": app.url_path_for})
+    return templates.TemplateResponse("index.html", {"request": request, "url_for": app.url_path_for, "root_path": app.root_path})
 
 
 # --- Analyze Endpoint ---
