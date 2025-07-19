84eb1b1d7361989396e67ef4b26cfed4bedbf214
Rahma-Alshafi <rahmaelshafe78@gmail.com>
2025-07-19 17:26:04 +0300

    postman

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ logging.basicConfig(
 )
 
 # --- FastAPI App Initialization ---
-root_path = ""
+root_path = "/spaces/Rahma07/ATS"
 app = FastAPI(
     title=settings.PROJECT_NAME,
     description=settings.PROJECT_DESCRIPTION,
